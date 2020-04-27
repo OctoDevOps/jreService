db56b36e37b9c64ea91eb79863320d874d907181
Dinesh Ganesan <dineshganesan@Dineshs-MBP-2.fios-router.home>
2020-04-26 20:20:16 -0400

    initial add

---
+++ src/main/java/com/acarin/jreService/JreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.acarin.jreService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acarin/jreService/JreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acarin.jreService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
